078c8589e4640f4d3e41eef6512c51943af56ec2
daria karelina <166645643+DariaKarelina@users.noreply.github.com>
2024-11-22 14:29:58 +0300

    Add files via upload

---
+++ src/Menu.js
@@ -0,0 +1,37 @@
+// import { Router } from 'react-router-dom';
+import './Menu.css';
+import Home from './Home/Page/home1Home';
+import Notes from './Notes/page1/notes1Notes';
+
+const Menu = () => {
+    return(
+        <div className="menu"> 
+            <div className="menu-home"> 
+                <a href="/home">
+                    
+                        Главная
+                    
+                </a> 
+            </div> 
+
+            <div className="menu-notes"> 
+                <a href="/notes"> Конспекты </a> 
+            </div> 
+
+            <div className="menu-hw"> 
+                <a href="/homework"> Домашнее задание </a> 
+            </div> 
+
+            <div className="menu-self"> 
+                <a href="/self"> Самостоятельное изучение </a> 
+            </div> 
+
+            <div className="menu-about"> 
+                <a href="/about"> О курсе </a> 
+            </div> 
+
+    </div> 
+); 
+};
+
+export default Menu;
